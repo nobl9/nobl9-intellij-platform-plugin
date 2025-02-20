4acd29dd5be1d6d92611e9e9f228dcfdcca8d21b
GitHub Action <action@github.com>
2025-02-20 18:35:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/nieomylnieja/nobl9intellijplatformplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nieomylnieja/nobl9intellijplatformplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nieomylnieja.nobl9intellijplatformplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
